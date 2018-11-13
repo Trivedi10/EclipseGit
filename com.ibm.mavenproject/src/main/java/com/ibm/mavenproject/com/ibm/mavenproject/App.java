7b6859e66b806cccfc1266406dc4cb902d66154b
Trivedi10 <kapilrajtrivedi.nmims@gmail.com>
2018-11-13 17:56:48 +0530

    Eclipse Commit 1

---
+++ com.ibm.mavenproject/src/main/java/com/ibm/mavenproject/com/ibm/mavenproject/App.java
@@ -0,0 +1,9 @@
+package com.ibm.mavenproject.com.ibm.mavenproject;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
